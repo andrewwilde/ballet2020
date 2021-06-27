bb5addd6b2ea2dc6508ad9ebc44801d875020595
Andrew Wilde <andrew.wilde@gmail.com>
2021-06-27 09:44:36 -0600

    Updated email to be generic for classes or camps.

--- payment/templates/email.py
+++ payment/templates/email.py
@@ -2,7 +2,7 @@ def get_confirm_html_template():
     return  """
 {parent_first},
 
-<p>Thank you for registering with Petit Ballet Academy!! We are so excited to have you join us for dance camp this year (or class if you're an adult)!</p> 
+<p>Thank you for registering with Petit Ballet Academy!! We are so excited to have you join us for dance this year!</p> 
 
 <p>{student_first} is currently registered for the following camp or classes:</p>
 
@@ -10,7 +10,7 @@ def get_confirm_html_template():
 
 <p>Our studio location is 478 South, 2000 West Springville, UT 84663 Unit #111. It's West of the Cracker Barrel right across the street.</p> 
 
-<p>Attached are all of our updated policies for the 2020-2021 dance year. Please look over them and bring the COVID-19 policy as well as the Liability Waiver signed on the first day of camp/class.</p> 
+<p>Attached are all of our updated policies for the 2020-2021 dance year. Please look over them and bring the Liability Waiver signed on the first day of class.</p> 
 
 <p>If you have any questions or concerns, please don't hesitate to reach out to us via text, email, or phone. We want this to be a wonderful experience for you, as the parents, as well as your children. We look forward to getting to know you better throughout the year.</p> 
 
@@ -26,7 +26,7 @@ def get_confirm_text_template():
     return """
 {parent_first},
 
-Thank you for registering with Petit Ballet Academy!! We are so excited to have you join us for dance camp this year (or class if you're an adult)! 
+Thank you for registering with Petit Ballet Academy!! We are so excited to have you join us for dance this year! 
 
 {student_first} is currently registered for the following camp or classes:
 
@@ -34,7 +34,7 @@ Thank you for registering with Petit Ballet Academy!! We are so excited to have 
 
 Our studio location is 478 South, 2000 West Springville, UT 84663 Unit #111. It's West of the Cracker Barrel right across the street. 
 
-Attached are all of our updated policies for the 2020-2021 dance year. Please look over them and bring the COVID-19 policy as well as the Liability Waiver signed on the first day of camp/class. 
+Attached are all of our updated policies for the 2020-2021 dance year. Please look over them and bring the Liability Waiver signed on the first day of camp/class. 
 
 If you have any questions or concerns, please don't hesitate to reach out to us via text, email, or phone. We want this to be a wonderful experience for you, as the parents, as well as your children. We look forward to getting to know you better throughout the year. 
 
